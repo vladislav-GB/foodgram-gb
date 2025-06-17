cc1cb7913ff815e8a5bff6a681d8b6be0690e277
vladislav <avengus.gb@gmail.com>
2025-06-17 11:20:53 +0700

    Правки Backend

--- backend/foodgram/api/tests.py
+++ backend/foodgram/api/tests.py
@@ -1,12 +1,1 @@
-from django.test import TestCase
-from rest_framework import status
-from rest_framework.test import APIClient
 
-
-class APISmokeTests(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-
-    def test_api_root_exists(self):
-        response = self.client.get("/api/")
-        self.assertIn(response.status_code, [200, 301, 302], "API root не доступен")
